8802ace30abbf5cb75104b810d71db946d9f2deb
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-05-04 10:54:08 -0300

    Install TailwindCSS and initialize the project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
+
