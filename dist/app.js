817e04f1c160c540bfadd633edc94be3a9cdc363
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-05-09 12:55:43 -0300

    Change styles and edit personal information

--- dist/app.js
+++ dist/app.js
@@ -2,13 +2,13 @@
 
 const menuButton = document.querySelector('#menu-button');
 const menuItems = document.querySelector('#menu');
-const menuButtonSpans = document.querySelectorAll('#menu-button span');
+const menuButtonSpans = document.querySelectorAll('#menu-button span');     // querySelectorAll porque son 3 spans 
 const links = document.querySelectorAll('#menu a');
 
 /****************************** ABRIR MENU ******************************/
 
-menuButton.addEventListener('click', ()=>{
-    menuItems.classList.toogle('hidden');                // Al hacer click, se coloca y saca la clase
+menuButton.addEventListener('click', () => {
+    menuItems.classList.toogle('hidden');                // Al hacer click, se despliega el menu hamburguesa. Cuando se clickea nuevamente, se oculta el menu
     menuButtonSpans.forEach((span) => {
         span.classList.toogle('animado');
     });
@@ -17,8 +17,8 @@ menuButton.addEventListener('click', ()=>{
 /****************************** Cerrar menu al hacer click en alguno de los enlaces ******************************/
 
 links.forEach((link)=>{
-    link.addEventListener('click', ()=>{
-        menuItems.classList.add('hidden');
+    link.addEventListener('click', () => {
+        menuItems.classList.add('hidden');                // Se cierra el menu
         menuButtonSpans.forEach((span) =>{
             span.classList.remove('animado');             // Para cada span, se elimina la clase animado 
         });
@@ -30,12 +30,12 @@ links.forEach((link)=>{
 window.onresize = function () {
     let w = window.outerWidth;
     if(w > 768){
-        menuItems.classList.add('hidden');
+        // menuItems.classList.add('hidden');
         menuButtonSpans.forEach((span) => {
             span.classList.remove('animado');
         });
     }
-}; 
+};
 
 /****************************** DARK MODE ******************************/
 
@@ -66,18 +66,19 @@ darkButton.addEventListener('click', function() {
 
 /****************************** VARIABLES JOB SECTION ******************************/
 
+/* 
 const jobSection = document.querySelector('#jobSection');
 const articles = document.querySelectorAll('#jobSection article');
 
 jobSection.addEventListener('click', (event)=>{
     const id = event.target.dataset.id; // Representa elementos sobre el cual hagamos click (cuando se hace click en cada trabajo, eso representa un event target)
 
-    /* Enlazar id con la descripcion del trabajo */
+    // Enlazar id con la descripcion del trabajo 
     if(id){
         articles.forEach((article)=>{
             article.classList.add('hidden'); // Ocultamiento de secciones porque solo se necesita mostrtar una 
         });
-        /* Que descripcion hay que mostrar */
+        // Que descripcion hay que mostrar 
         const element = document.getElementById(id);
         element.classList.remove('hidden');
     }
@@ -91,3 +92,5 @@ jobLinks.forEach((link) => {
         firstLink.classList.remove('border-sky-500', 'bg-white', 'dark:bg-slate-700');
     });
 });
+
+*/
